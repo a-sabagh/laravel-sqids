da004c0705699f4c9c89d530b1c219ccfbf8c9f1
a-sabagh <a.sabagh72@gmail.com>
2025-08-20 16:32:17 +0330

    package config

---
+++ config/sqids.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => env('SQIDS_DEFAULT', 'default'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hashids Drivers
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the drivers setup for your application. Example
+    | configuration has been included, but you may add as many drivers as
+    | you would like.
+    |
+    */
+
+    'drivers' => [
+
+        'default' => [
+            'pad' => env('SQIDS_DEFAULT_PAD', ''),
+            'length' => env('SQIDS_DEFAULT_LENGTH', '6'),
+            // 'blocklist' => env('SQIDS_DEFAULT_BLOCK_LIST', []),
+            // 'alphabet' => env('SQIDS_DEFAULT_ALPHABET', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'),
+        ],
+
+        'tracking' => [
+            'pad' => env('SQIDS_TRACKING_PAD', '5000'),
+            'length' => env('SQIDS_TRACKING_LENGTH', '8'),
+            'alphabet' => env('SQIDS_TRACKING_ALPHABET', '1234567890'),
+        ],
+
+    ],
+
+];
