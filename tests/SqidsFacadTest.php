fa647fbdabb850d08a4ae093e5cfd4b1b4641ff6
a-sabagh <a.sabagh72@gmail.com>
2025-08-21 13:17:05 +0330

    test sqids encoder accept singular number

--- tests/SqidsFacadTest.php
+++ tests/SqidsFacadTest.php
@@ -4,14 +4,32 @@ namespace LaravelSqids\Tests;
 
 use LaravelSqids\Facades\Sqids;
 use LaravelSqids\SqidsManager;
+use LaravelSqids\SqidsServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 class SqidsFacadTest extends TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [
+            SqidsServiceProvider::class,
+        ];
+    }
+
     public function test_sqids_facade_accessor_correctly_bind_to_manager(): void
     {
         $facadeRoot = Sqids::getFacadeRoot();
 
         $this->assertInstanceOf(SqidsManager::class, $facadeRoot);
     }
+
+    public function test_sqids_encoder_accept_integer(): void
+    {
+        $id = fake()->numberBetween(10, 1000);
+
+        $encodeString = Sqids::encode($id);
+
+        $this->assertIsString($encodeString);
+    }
+
 }
