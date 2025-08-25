2df3cc60fbec729d86f1ab06257645b375bfb50d
a-sabagh <a.sabagh72@gmail.com>
2025-08-25 13:01:27 +0330

    fairly validate test

--- tests/SqidsValidationRuleTest.php
+++ tests/SqidsValidationRuleTest.php
@@ -2,15 +2,20 @@
 
 namespace LaravelSqids\Tests;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use LaravelSqids\Facades\Sqids;
-use Illuminate\Support\Facades\Validator;
 use LaravelSqids\Validations\SqidsValidationRule;
 
 class SqidsValidationRuleTest extends TestCase
 {
     public function test_default_sqids_validation_rule(): void
     {
+        $this->app->config->set('sqids.drivers.default', [
+            'alphabet' => '!@#$%^&*()_+-|',
+            'length' => 5,
+        ]);
+
         $id = fake()->numberBetween(10, 99999);
 
         $validator = Validator::make(
